7a4738ad11af771d7aa37aaac4c8f5b0aba33b2b
erikaanne9 <erikaanne9@gmail.com>
2022-04-18 20:20:01 -0500

    Added time function in js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#big-temp");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].main;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 
   let humidityElement = document.querySelector(".humidity");
   humidityElement.innerHTML = `Humidity: ${Math.round(
@@ -15,6 +38,9 @@ function displayTemperature(response) {
 
   let cityElement = document.querySelector(".weather-heading");
   cityElement.innerHTML = `${response.data.name} Weather Conditions`;
+
+  let dateElement = document.querySelector(".date-time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b1b855fb121f5178057b1006a42c2d0b";
